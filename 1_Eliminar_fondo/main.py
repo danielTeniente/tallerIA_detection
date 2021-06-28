a455901c4aeb7e5d095e6e7201d934aa5e2402ad
Daniel Diaz <daniel.diaz.bedoya@outlook.com>
2021-06-28 14:05:39 -0500

    Tema 1: eliminar fondo

--- 1_Eliminar_fondo/main.py
+++ 1_Eliminar_fondo/main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import math
 import pandas as pd
 
 # Abrir el archivo del video
@@ -12,15 +11,83 @@ if not capture.isOpened:
     print('Unable to open: ' + path)
     exit(0)
 
+################################
+# MODULO DE ELIMINAR FONDO
+################################
+# 2
+#backSub = cv2.createBackgroundSubtractorMOG2(history=300, detectShadows=False)
+################################
+################################
+
+#Kernel
+# 4
+#menores a 5,5 produce muchos bordes
+#kernel_close = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 5))
+#kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+
 while True:
     ret, frame = capture.read()
     if frame is None:
         break  
 
-    cv2.imshow('Frame', frame)
+    ##################################
+    # CAMBIAR ALTURA Y ANCHO
+    ###################################
+    # 1
+    #height , width , layers =  frame.shape
+    #resized_frame = cv2.resize(frame, (width//3, height//3)) 
+    ###################################
 
-    #cerrar el programa
+    #-------------------------------------------------
+    ##################################
+    # ELIMINAR FONDO
+    # apply(input,output,learning_rate)
+    ##################################
+    # 2
+    #back_mask = backSub.apply(resized_frame,None, 0.003)
+    # 3
+    #fgMask = cv2.GaussianBlur(resized_frame, (5, 5), 0)
+    #fgMask = backSub.apply(fgMask,None, 0.003)
+    ##################################
+
+    #-----------------------------------------------------
+    ##################################
+    # Filtros
+    ##################################
+    
+    # 4
+    # Remove noise
+    #opening = cv2.morphologyEx(fgMask, cv2.MORPH_OPEN, kernel_close)
+
+    # Fill any small holes
+    #closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+
+    # Dilate to merge adjacent blobs
+    #dilation = cv2.dilate(closing, kernel, iterations=3)
+
+    # threshold
+    #dilation[dilation < 250] = 0    
+
+    #-----------------------------------------------------
+    ##################################
+    # MOSTRAR LA IMAGEN
+    ##################################
+    cv2.imshow('Frame_original', frame)
+    # 1
+    #cv2.imshow('Frame_menor', resized_frame)
+    # 2
+    #cv2.imshow('Frame_mascara', back_mask)
+    # 3
+    #cv2.imshow('Frame_mascara_blur', fgMask)
+    # 4
+    #cv2.imshow('Frame_mascara_filtros', dilation)
+
+    #--------------------------------------------------
+    #cerrar el programa al presionar Esc
     keyboard = cv2.waitKey(30)
+    ##################################
+    ##################################
 
     if keyboard == 'q' or keyboard == 27:
         break      
